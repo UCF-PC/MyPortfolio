b9ca60d3572e4d8a8adb120e0ba9b6b58b086d38
shoqa <shoqa@UCF>
2022-04-23 21:01:33 +0300

    Add project files.

---
+++ Core/Entities/Address.cs
@@ -0,0 +1,9 @@
+﻿namespace Core.Entities
+{
+    public class Address : EntityBase
+    {
+        public string Street { get; set; }
+        public string Cit { get; set; }
+        public int Number { get; set; }
+    }
+}
---
+++ Core/Entities/EntityBase.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Core.Entities
+{
+    public class EntityBase
+    {
+        public Guid MyProperty { get; set; }
+    }
+}
